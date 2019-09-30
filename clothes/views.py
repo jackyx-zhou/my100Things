e2909311f432b29894039a70e2ddbb9889cda871
j87031yz <yuxuan.zhou@student.manchester.ac.uk>
2019-09-30 22:34:44 +0100

    stable with backend skeleton

--- clothes/views.py
+++ clothes/views.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from clothes.models import Clothing
+from clothes.serializers import ClothingSerializer
+from rest_framework import generics
+class ClothingListCreate(generics.ListCreateAPIView):
+    queryset = Clothing.objects.all()
+    serializer_class = ClothingSerializer
